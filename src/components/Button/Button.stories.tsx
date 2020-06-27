03a0f44fd6db0fc461a11f9931c718eb2452ab19
Konstantin Komelin <konstantin.komelin@gmail.com>
2020-06-27 22:11:04 +0300

    Added styles for the Button component.

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -10,32 +10,40 @@ export default {
 
 export const sizes = () => (
   <>
-    <Button onClick={action('clicked')} size='normal'>
-      Normal
-    </Button>
-    <br/>
-    <Button onClick={action('clicked')} size='small'>
-      Small
-    </Button>
-    <br/>
-    <Button onClick={action('clicked')} size='big'>
-      Big
-    </Button>
+    <div style={{marginBottom: "2rem", textAlign: 'center'}}>
+      <Button onClick={action('clicked')} size='small'>
+        Small
+      </Button>
+    </div>
+    <div style={{marginBottom: "2rem", textAlign: 'center'}}>
+      <Button onClick={action('clicked')} size='normal'>
+        Normal
+      </Button>
+    </div>
+    <div style={{marginBottom: "2rem", textAlign: 'center'}}>
+      <Button onClick={action('clicked')} size='big'>
+        Big
+      </Button>
+    </div>
   </>
 );
 
 export const disabled = () => (
   <>
-    <Button onClick={action('clicked')} disabled size='normal'>
-      Normal Disabled
-    </Button>
-    <br/>
-    <Button onClick={action('clicked')} disabled size='small'>
-      Small Disabled
-    </Button>
-    <br/>
-    <Button onClick={action('clicked')} disabled size='big'>
-      Big Disabled
-    </Button>
+    <div style={{marginBottom: "2rem", textAlign: 'center'}}>
+      <Button onClick={action('clicked')} disabled size='small'>
+        Small Disabled
+      </Button>
+    </div>
+    <div style={{marginBottom: "2rem", textAlign: 'center'}}>
+      <Button onClick={action('clicked')} disabled size='normal'>
+        Normal Disabled
+      </Button>
+    </div>
+    <div style={{marginBottom: "2rem", textAlign: 'center'}}>
+      <Button onClick={action('clicked')} disabled size='big'>
+        Big Disabled
+      </Button>
+    </div>
   </>
 );
