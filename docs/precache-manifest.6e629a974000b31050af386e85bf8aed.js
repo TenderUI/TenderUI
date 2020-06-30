ab6dd5ba82017ad9129fe6892d9a1a1c378051d4
Konstantin Komelin <konstantin.komelin@gmail.com>
2020-06-30 21:45:00 +0300

    Changed storybook build folder to docs to be able to use Github pages for hosting it.

---
+++ docs/precache-manifest.6e629a974000b31050af386e85bf8aed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1a9e085461d6d402940460724621e7",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.1f3f7600fa6d5e1ea517.bundle.js"
+  },
+  {
+    "url": "runtime~main.1f3f7600fa6d5e1ea517.bundle.js"
+  },
+  {
+    "revision": "cf543d1067c8b22f9ece",
+    "url": "static/css/main.0697ef39.chunk.css"
+  },
+  {
+    "url": "vendors~main.1f3f7600fa6d5e1ea517.bundle.js"
+  },
+  {
+    "url": "vendors~main.1f3f7600fa6d5e1ea517.bundle.js.LICENSE.txt"
+  }
+]);
