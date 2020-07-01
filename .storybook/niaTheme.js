e1a9fc3ef4f19658402e57e7d7a0f3d0a5ed80dc
Konstantin Komelin <konstantin.komelin@gmail.com>
2020-07-01 10:24:05 +0300

    Changed storybook project name.

--- .storybook/niaTheme.js
+++ .storybook/niaTheme.js
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming/create';
 export default create({
   base: 'light',
 
-  brandTitle: 'Nia Style Guide',
+  brandTitle: 'Nia Components',
   brandUrl: 'https://github.com/kkomelin/nia',
   // brandImage: 'https://placehold.it/350x150',
 });
